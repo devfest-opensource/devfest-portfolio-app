ab2d620379dfb4c8275fbd37cd1b7351954107f3
“Nabhya <svirajhack3r@gmail.com>
2022-12-01 22:59:45 +0530

    SideBar Built

--- lib/presentation/home-screen/home_screen.dart
+++ lib/presentation/home-screen/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:devfest_portfolio_app/presentation/home-screen/widgets/side_bar.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -5,9 +6,12 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Home Screen"),
+    return Scaffold(
+      body: Row(
+        children: const [
+          SideBar(),
+          // This is where rest of the home UI will Go.
+        ],
       ),
     );
   }
