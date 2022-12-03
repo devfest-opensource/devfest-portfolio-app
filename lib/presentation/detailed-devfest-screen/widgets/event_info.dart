9077a5317cc1f97e6af1d1a6e2fd4ef4e724e522
“Nabhya <svirajhack3r@gmail.com>
2022-12-04 04:20:34 +0530

    :construction: Details Page Initiated

---
+++ lib/presentation/detailed-devfest-screen/widgets/event_info.dart
@@ -0,0 +1,84 @@
+import 'package:devfest_portfolio_app/presentation/detailed-devfest-screen/widgets/organizer_card.dart';
+import 'package:devfest_portfolio_app/presentation/detailed-devfest-screen/widgets/speaker_card.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../entities/home_data_entity.dart';
+import '../../../utils/colors.dart';
+import '../../../utils/textstyle.dart';
+
+class EventInfo extends StatelessWidget {
+  const EventInfo({Key? key, required this.homeDataEntity}) : super(key: key);
+
+  final HomeDataEntity homeDataEntity;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: 10),
+      padding: const EdgeInsets.fromLTRB(20, 20, 20, 20),
+      width: 295.w,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(5.r),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.shade300,
+            offset: const Offset(2, 0),
+            blurRadius: 5,
+            spreadRadius: 0.05,
+          ),
+          BoxShadow(
+            color: Colors.grey.shade300,
+            offset: const Offset(-2, 5),
+            blurRadius: 5,
+            spreadRadius: 0.05,
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Speakers",
+            style: subHeading.copyWith(
+              color: blueShade,
+            ),
+          ),
+          Container(
+            color: blueShade,
+            height: 2.h,
+          ),
+          ListView.builder(
+            primary: false,
+            itemCount: homeDataEntity.speakers.length,
+            itemBuilder: (context, index) => SpeakerCard(speaker: homeDataEntity.speakers[index],),
+            shrinkWrap: true,
+          ),
+          SizedBox(height: 20.h,),
+          Text(
+            "Organizers",
+            style: subHeading.copyWith(
+              color: blueShade,
+            ),
+          ),
+          Container(
+            color: blueShade,
+            height: 2.h,
+          ),
+          SizedBox(
+            height: 200.w,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              primary: false,
+              itemCount: homeDataEntity.organizers.length,
+              itemBuilder: (context, index) => OrganizerCard(organizer: homeDataEntity.organizers[index]),
+              shrinkWrap: true,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/presentation/detailed-devfest-screen/widgets/speaker_card.dart
@@ -0,0 +1,83 @@
+import 'package:devfest_portfolio_app/entities/home_data_entity.dart';
+import 'package:devfest_portfolio_app/utils/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../utils/textstyle.dart';
+
+class SpeakerCard extends StatelessWidget {
+  const SpeakerCard({Key? key, required this.speaker}) : super(key: key);
+
+  final Speaker speaker;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      margin: const EdgeInsets.only(top: 10),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(5.r),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.shade300,
+            offset: const Offset(2, 0),
+            blurRadius: 5,
+            spreadRadius: 0.05,
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              CircleAvatar(
+                radius: 25,
+                child: ClipOval(
+                  child: Image.network("${speaker.image}"),
+                ),
+              ),
+              const SizedBox(width: 10,),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    width: 180.w,
+                    child: Text(
+                      speaker.name,
+                      style: subHeading.copyWith(
+                        color: blueShade,
+                        fontWeight: FontWeight.w300
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 180.w,
+                    child: Text(
+                      speaker.title,
+                      style: subHeading.copyWith(
+                        color: Colors.grey,
+                        fontSize: 12.sp,
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+          const SizedBox(height: 10,),
+          Text(
+            speaker.designation,
+            style: subHeading.copyWith(
+              color: Colors.grey,
+              fontSize: 12.sp,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
